29d4619c76a89660e0e97c1f4f514832b3e52b36
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-11-10 17:59:00 +0300

    Added Recycle Bin View

---
+++ P1_PCleaner/View/RecycleBinView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace P1_PCleaner.View;
+
+public partial class RecycleBinView : UserControl
+{
+    public RecycleBinView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ P1_PCleaner/ViewModel/RecycleBinViewModel.cs
@@ -0,0 +1,20 @@
+﻿using P1_PCleaner.Command;
+using P1_PCleaner.Util;
+
+namespace P1_PCleaner.ViewModel;
+
+public class RecycleBinViewModel : ObservableObject
+{
+    public double TotalGb { get; }
+    public long FilesCount { get; }
+
+    public RelayCommand OpenRecycleBin => new(_ => RecycleBin.OpenRecycleBinFolder());
+
+    public RecycleBinViewModel()
+    {
+        var info = RecycleBin.GetInfo();
+        TotalGb = info.GSize;
+        FilesCount = info.FileCount;
+
+    }
+}
