7e6dcdba522896f7d22a347ff315df970316a784
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-11-10 18:02:56 +0300

    Added view model for main view

---
+++ P1_PCleaner/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using P1_PCleaner.Command;
+using P1_PCleaner.Util;
+
+namespace P1_PCleaner.ViewModel;
+
+public class MainViewModel : ObservableObject
+{
+    private object? _currentViewModel;
+
+    public object? CurrentViewModel 
+    {
+        get => _currentViewModel;
+        set
+        {
+            _currentViewModel = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainViewModel()
+    {
+        _currentViewModel = new ScanViewModel();
+    }
+
+    public RelayCommand<ViewType> ChangeView => new(ChangeViewModel);
+
+    public void ChangeViewModel(ViewType type)
+    {
+        CurrentViewModel = type switch
+        {
+            ViewType.Scanner => new ScanViewModel(),
+            ViewType.RecycleBin => new RecycleBinViewModel(),
+            _ => new NotFoundViewModel()
+        };
+    }
+}
