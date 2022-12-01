a3faf93837e408790d2bae3fa08c318c351bfce7
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-01 20:35:44 +0300

    Added new controls

---
+++ P1_PCleaner/Control/CategoryCheckBox.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace P1_PCleaner.Control;
+
+public class CategoryCheckBox : CheckBox
+{
+    public static readonly DependencyProperty SizeProperty = DependencyProperty.Register(nameof(Size),
+        typeof(double), typeof(CategoryCheckBox),
+        new PropertyMetadata(0.0, OnSizePropertyChanged));
+
+    public static readonly DependencyProperty SizeSuffixProperty = DependencyProperty.Register(nameof(SizeSuffix),
+        typeof(string), typeof(CategoryCheckBox),
+        new PropertyMetadata("B", OnSizeSuffixPropertyChanged));
+
+    public double Size
+    {
+        get => (double)GetValue(SizeProperty);
+        set => SetValue(SizeProperty, value);
+    }
+
+    public string SizeSuffix
+    {
+        get => (string)GetValue(SizeSuffixProperty);
+        set => SetValue(SizeSuffixProperty, value);
+    }
+
+    static CategoryCheckBox()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CategoryCheckBox),
+            new FrameworkPropertyMetadata(typeof(CategoryCheckBox)));
+    }
+
+    private static void OnSizePropertyChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+    {
+        d.SetValue(SizeProperty, e.NewValue);
+    }
+
+    private static void OnSizeSuffixPropertyChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+    {
+        d.SetValue(SizeSuffixProperty, e.NewValue);
+    }
+}
---
+++ P1_PCleaner/Control/CategoryRadioButton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace P1_PCleaner.Control;
+
+public class CategoryRadioButton : RadioButton
+{
+    public static readonly DependencyProperty CategorySizeProperty = DependencyProperty.Register(nameof(CategorySize),
+        typeof(double), typeof(CategoryRadioButton),
+        new PropertyMetadata(0.0, OnCategorySizePropertyChanged));
+
+    public static readonly DependencyProperty CategoryIdentificatorProperty = DependencyProperty.Register(
+        nameof(CategoryIdentificator),
+        typeof(string), typeof(CategoryRadioButton),
+        new PropertyMetadata("B", OnCategoryIdentificatorPropertyChanged));
+    
+    public double CategorySize
+    {
+        get => (double)GetValue(CategorySizeProperty);
+        set => SetValue(CategorySizeProperty, value);
+    }
+
+    public string CategoryIdentificator
+    {
+        get => (string)GetValue(CategoryIdentificatorProperty);
+        set => SetValue(CategoryIdentificatorProperty, value);
+    }
+
+    static CategoryRadioButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CategoryRadioButton), new
+            FrameworkPropertyMetadata(typeof(CategoryRadioButton)));
+    }
+
+    private static void OnCategorySizePropertyChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+    {
+        d.SetValue(CategorySizeProperty, e.NewValue);
+    }
+
+    private static void OnCategoryIdentificatorPropertyChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
+    {
+        d.SetValue(CategoryIdentificatorProperty, e.NewValue);
+    }
+}
