682f59041401b5845bc006748a77a85397254458
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-01 20:34:44 +0300

    Added command bases

---
+++ P1_PCleaner/Command/AsyncCommandBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace P1_PCleaner.Command;
+
+public abstract class AsyncCommandBase : CommandBase
+{
+    public bool IsExecuting { get; private set; }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return !IsExecuting && base.CanExecute(parameter);
+    }
+
+    public override async void Execute(object? parameter)
+    {
+        IsExecuting = true;
+        try
+        {
+            await ExecuteAsync(parameter);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+        finally
+        {
+            IsExecuting = false;
+        }
+    }
+
+    public abstract Task ExecuteAsync(object? parameter);
+}
