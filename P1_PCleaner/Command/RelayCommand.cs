706578ad131a38c9574e1a488918d4b70ed48b38
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-01 20:36:52 +0300

    Updated relay command

--- P1_PCleaner/Command/RelayCommand.cs
+++ P1_PCleaner/Command/RelayCommand.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Windows.Input;
+using System.Threading.Tasks;
 
 namespace P1_PCleaner.Command;
 
-public class RelayCommand<T> : ICommand
+public class RelayCommand<T> : CommandBase
 {
     private readonly Action<T> _action;
     private readonly Func<T?, bool>? _canExecute;
@@ -15,50 +15,85 @@ public class RelayCommand<T> : ICommand
     }
 
 
-    public bool CanExecute(object? parameter)
+    public override bool CanExecute(object? parameter)
     {
         if (parameter is not T src) return false;
-        return _canExecute == null || _canExecute(src);
+        return (_canExecute == null || _canExecute(src)) && base.CanExecute(parameter);
     }
 
-    public void Execute(object? parameter)
+    public override void Execute(object? parameter)
     {
         if (parameter is not T src) return;
         _action(src);
     }
-
-    public event EventHandler? CanExecuteChanged
-    {
-        add => CommandManager.RequerySuggested += value;
-        remove => CommandManager.RequerySuggested -= value;
-    }
 }
 
-public class RelayCommand : ICommand
+public class RelayCommand : CommandBase
 {
-    private readonly Action<object> _action;
+    private readonly Action<object?> _action;
     private readonly Func<object?, bool>? _canExecute;
 
-    public RelayCommand(Action<object> action, Func<object?, bool>? canExecute = null)
+    public RelayCommand(Action<object?> action, Func<object?, bool>? canExecute = null)
     {
         _action = action;
         _canExecute = canExecute;
     }
 
 
-    public bool CanExecute(object? parameter)
+    public override bool CanExecute(object? parameter)
     {
-        return _canExecute == null || _canExecute(parameter);
+        return (_canExecute == null || _canExecute(parameter)) && base.CanExecute(parameter);
     }
 
-    public void Execute(object? parameter)
+    public override void Execute(object? parameter)
     {
         _action(parameter);
     }
+}
 
-    public event EventHandler? CanExecuteChanged
+public class AsyncRelayCommand<T> : AsyncCommandBase
+{
+    private readonly Action<T> _action;
+    private readonly Func<T?, bool>? _canExecute;
+
+    public AsyncRelayCommand(Action<T> action, Func<T?, bool>? canExecute = null)
     {
-        add => CommandManager.RequerySuggested += value;
-        remove => CommandManager.RequerySuggested -= value;
+        _action = action;
+        _canExecute = canExecute;
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        if (parameter is not T src) return false;
+        return (_canExecute == null || _canExecute(src)) && base.CanExecute(parameter);
+    }
+
+    public override Task ExecuteAsync(object? parameter)
+    {
+        if (parameter is not T src) return Task.CompletedTask;
+        _action(src);
+        return Task.CompletedTask;
+    }
+}
+
+public class AsyncRelayCommand : AsyncCommandBase
+{
+    private readonly Action<object?> _action;
+    private readonly Func<object?, bool>? _canExecute;
+
+    public AsyncRelayCommand(Action<object?> action, Func<object?, bool>? canExecute = null)
+    {
+        _action = action;
+        _canExecute = canExecute;
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return (_canExecute == null || _canExecute(parameter)) && base.CanExecute(parameter);
+    }
+
+    public override async Task ExecuteAsync(object? parameter)
+    {
+        await Task.Run(() => _action(parameter));
     }
 }
