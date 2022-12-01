68338824ddd50d31c39a4ae4fdb297d26a7a00d9
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-01 20:37:08 +0300

    Updated recycle view

--- P1_PCleaner/ViewModel/RecycleBinViewModel.cs
+++ P1_PCleaner/ViewModel/RecycleBinViewModel.cs
@@ -1,20 +1,27 @@
-﻿using P1_PCleaner.Command;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using P1_PCleaner.Command;
 using P1_PCleaner.Util;
 
 namespace P1_PCleaner.ViewModel;
 
 public class RecycleBinViewModel : ObservableObject
 {
-    public double TotalGb { get; }
-    public long FilesCount { get; }
+    public RecycleBin.Info Info { get; private set; }
 
     public RelayCommand OpenRecycleBin => new(_ => RecycleBin.OpenRecycleBinFolder());
+
+    public AsyncRelayCommand ClearRecycleBin => new(_ =>
+    {
+        RecycleBin.Clear();
+        var info = RecycleBin.GetInfo();
+        Info = info;
+        OnPropertyChanged(nameof(Info));
+    });
 
     public RecycleBinViewModel()
     {
         var info = RecycleBin.GetInfo();
-        TotalGb = info.GSize;
-        FilesCount = info.FileCount;
-
+        Info = info;
     }
 }
