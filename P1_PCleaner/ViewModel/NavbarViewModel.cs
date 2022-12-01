fb7c6e87225e3fc329547fb0320cbef2ea2b939a
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-01 20:37:50 +0300

    Added NavbarViewModel.cs

---
+++ P1_PCleaner/ViewModel/NavbarViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using P1_PCleaner.Command;
+using P1_PCleaner.Util;
+
+namespace P1_PCleaner.ViewModel;
+
+public class NavbarViewModel : ObservableObject
+{
+    private ObservableObject? _currentViewModel;
+
+    public ObservableObject? CurrentViewModel
+    {
+        get => _currentViewModel;
+        set
+        {
+            _currentViewModel = value;
+            ViewModelChanged?.Invoke();
+        }
+    }
+
+    public event Action? ViewModelChanged;
+
+    public RelayCommand<string> OpenUrl => new(uri =>
+    {
+        Process.Start(new ProcessStartInfo(uri) { UseShellExecute = true });
+    });
+
+    public RelayCommand<ViewType> ChangeView => new(ChangeViewModel);
+
+    public void ChangeViewModel(ViewType type)
+    {
+        CurrentViewModel = type switch
+        {
+            ViewType.Scanner => new ScanViewModel(),
+            ViewType.RecycleBin => new RecycleBinViewModel(),
+            ViewType.JunkFiles => new JunkFilesViewModel(),
+            _ => new NotFoundViewModel()
+        };
+    }
+}
