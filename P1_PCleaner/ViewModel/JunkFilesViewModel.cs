c8d312908b27f9708fdcae0d7cc190d6f3036b78
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 23:48:14 +0300

    Added commands and optimization

--- P1_PCleaner/ViewModel/JunkFilesViewModel.cs
+++ P1_PCleaner/ViewModel/JunkFilesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using P1_PCleaner.Command;
 using P1_PCleaner.Factory;
 using P1_PCleaner.Model;
@@ -8,37 +10,14 @@ using P1_PCleaner.Util;
 
 namespace P1_PCleaner.ViewModel;
 
-public class JunkFilesViewModel : ObservableObject
+public class JunkFilesViewModel : ViewModelBase
 {
+    private readonly NavbarViewModel _navbar;
     private readonly BrowserCacheCategoryViewModel _browserCacheCategoryViewModel;
     private readonly DownloadsCategoryViewModel _downloadsCategoryViewModel;
 
     private readonly WindowsSystemCategoryViewModel _windowsCategoryViewModel;
-    private ObservableObject? _currentJunkCategory;
-
-    public JunkFilesViewModel()
-    {
-        _windowsCategoryViewModel = new WindowsSystemCategoryViewModel(
-            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.SystemLogFiles),
-            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.SystemCache),
-            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.TempFiles));
-        _windowsCategoryViewModel.CategorySelected += _ => OnPropertyChanged(nameof(SelectedSize));
-
-        _browserCacheCategoryViewModel = new BrowserCacheCategoryViewModel(
-            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.GoogleChrome),
-            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.MozillaFirefox),
-            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.MicrosoftEdge)
-        );
-        _browserCacheCategoryViewModel.CategorySelected += _ => OnPropertyChanged(nameof(SelectedSize));
-
-        _downloadsCategoryViewModel =
-            new DownloadsCategoryViewModel(App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.Downloads));
-        _downloadsCategoryViewModel.CategorySelected += _ => OnPropertyChanged(nameof(SelectedSize));
-
-        CurrentJunkCategory = _windowsCategoryViewModel;
-
-        ChangeJunkCategory = new RelayCommand<JunkCategory>(ChangeCategory);
-    }
+    private ViewModelBase? _currentJunkCategory;
 
     public SizeInfo WindowsSystemSize => new FileSizeFactory().CreateInfo
     (
@@ -70,7 +49,7 @@ public class JunkFilesViewModel : ObservableObject
         new FileSizeFactory().CreateInfo(App.ScannedRepository.Categories().Values
             .Sum(category => category.SizeInfo.Length));
 
-    public ObservableObject? CurrentJunkCategory
+    public ViewModelBase? CurrentJunkCategory
     {
         get => _currentJunkCategory;
         set
@@ -81,6 +60,67 @@ public class JunkFilesViewModel : ObservableObject
     }
 
     public RelayCommand<JunkCategory> ChangeJunkCategory { get; }
+
+    public AsyncRelayCommand ClearSelectedFiles => new(_ =>
+    {
+        var loadingModel = new LoadingViewModel(_navbar, ViewType.JunkFiles);
+        _navbar.CurrentViewModel = loadingModel;
+        
+        var selectedCategories = App.ScannedRepository.Categories().Where(pair => pair.Value.IsSelected)
+            .Select(pair => pair.Key);
+        var selectedFiles = App.ScannedRepository.Categories().Values.Where(category => category.IsSelected)
+            .SelectMany(category => category.Files)
+            ;
+        foreach (var selectedCategory in selectedCategories)
+            App.ScannedRepository.Categories().Remove(selectedCategory);
+        
+        var deletedCount = 0;
+        var fileInfs = selectedFiles as FileInf[] ?? selectedFiles.ToArray();
+        _navbar.IsLocked = true;
+        loadingModel.IsLoading = true;
+        foreach (var inf in fileInfs)
+        {
+            deletedCount++;
+            loadingModel.Text = $"Удаление {inf.Name}";
+            loadingModel.Percent = (double)deletedCount / fileInfs.Length * 100;
+            File.Delete(inf.Path);
+            Thread.Sleep(1);
+        }
+
+        _navbar.IsLocked = false;
+        loadingModel.IsLoading = false;
+    });
+
+    public RelayCommand Scan => new(_ =>
+    {
+        App.ScannedRepository.Clear();
+        _navbar.ChangeViewModel(ViewType.Scanner);
+    });
+
+    public JunkFilesViewModel(NavbarViewModel navbar)
+    {
+        _navbar = navbar;
+        _windowsCategoryViewModel = new WindowsSystemCategoryViewModel(
+            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.SystemLogFiles),
+            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.SystemCache),
+            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.TempFiles));
+        _windowsCategoryViewModel.CategorySelected += _ => OnPropertyChanged(nameof(SelectedSize));
+
+        _browserCacheCategoryViewModel = new BrowserCacheCategoryViewModel(
+            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.GoogleChrome),
+            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.MozillaFirefox),
+            App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.MicrosoftEdge)
+        );
+        _browserCacheCategoryViewModel.CategorySelected += _ => OnPropertyChanged(nameof(SelectedSize));
+
+        _downloadsCategoryViewModel =
+            new DownloadsCategoryViewModel(App.ScannedRepository.GetCategory(IFilesRepository.ScanCategory.Downloads));
+        _downloadsCategoryViewModel.CategorySelected += _ => OnPropertyChanged(nameof(SelectedSize));
+
+        CurrentJunkCategory = _windowsCategoryViewModel;
+
+        ChangeJunkCategory = new RelayCommand<JunkCategory>(ChangeCategory);
+    }
 
     private void ChangeCategory(JunkCategory category)
     {
