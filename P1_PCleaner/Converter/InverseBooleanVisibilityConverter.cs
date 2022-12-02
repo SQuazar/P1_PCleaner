05c3521a0e9abc70bb31969a974ca9fa87f4c79f
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 23:47:26 +0300

    Added InverseBooleanVisibilityConverter.cs

---
+++ P1_PCleaner/Converter/InverseBooleanVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace P1_PCleaner.Converter;
+
+public class InverseBooleanVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is false ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
