63371b9bf22a03ec8a3c12e2d59de867d6862841
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 23:47:44 +0300

    New utility method and bug fixes

--- P1_PCleaner/IO/IScanner.cs
+++ P1_PCleaner/IO/IScanner.cs
@@ -17,11 +17,21 @@ public interface IScanner
     static IEnumerable<FileInf> GetFiles(DirectoryInfo directory, string[] ext, bool deep = true)
     {
         if (directory == null) throw new ArgumentNullException(nameof(directory));
+        if (ext == null) throw new ArgumentNullException(nameof(ext));
         IFileSizeFactory factory = new FileSizeFactory();
         return directory.EnumerateFiles("*.*", new EnumerationOptions
-                { RecurseSubdirectories = true, MatchType = MatchType.Win32, AttributesToSkip = 0 })
+                { RecurseSubdirectories = deep, MatchType = MatchType.Win32, AttributesToSkip = 0 })
             .Where(fileInfo => ext.Any(x => fileInfo.Extension.EndsWith(x) || x.Equals("*")))
             .Select(fileInfo => new FileInf
                 { Path = fileInfo.FullName, SizeInfo = factory.CreateInfo(fileInfo.Length) });
+    }
+
+    static IEnumerable<FileInf> GetFiles(DirectoryInfo[] directories, string[] ext, bool deep = false)
+    {
+        if (directories == null) throw new ArgumentNullException(nameof(directories));
+        var files = new List<FileInf>();
+        foreach (var directory in directories)
+            files.AddRange(GetFiles(directory, ext, deep));
+        return files;
     }
 }
