4f98663eb4e70ed9a48a1ca817dd64dc488828e7
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 20:43:51 +0300

    Added factory for SizeInfo.cs

---
+++ P1_PCleaner/Factory/FileSizeFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using P1_PCleaner.Model;
+
+namespace P1_PCleaner.Factory;
+
+public class FileSizeFactory : IFileSizeFactory
+{
+    public SizeInfo CreateInfo(long length)
+    {
+        double size = length;
+        string sizeSuffix;
+        if (size < 1024)
+            sizeSuffix = "B";
+        else if (size < Math.Pow(1024, 2))
+            sizeSuffix = "KB";
+        else if (size < Math.Pow(1024, 3))
+            sizeSuffix = "MB";
+        else if (size < Math.Pow(1024, 4))
+            sizeSuffix = "GB";
+        else if (size < Math.Pow(1024, 5))
+            sizeSuffix = "TB";
+        else sizeSuffix = "unresolved";
+        size = sizeSuffix switch
+        {
+            "B" => size,
+            "KB" => size / 1024,
+            "MB" => size / 1024 / 1024,
+            "GB" => size / 1024 / 1024 / 1024,
+            "TB" => size / 1024 / 1024 / 1024 / 1024,
+            _ => 0
+        };
+        return new SizeInfo { Size = size, Length = length, SizeSuffix = sizeSuffix };
+    }
+}
