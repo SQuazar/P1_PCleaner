e00106d3991506345167c61ddc0153a070f6d0f4
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 20:56:59 +0300

    Added Category class

---
+++ P1_PCleaner/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using P1_PCleaner.Factory;
+
+namespace P1_PCleaner.Model;
+
+public class Category
+{
+    public List<FileInf> Files { get; } = new();
+    public SizeInfo SizeInfo => new FileSizeFactory().CreateInfo(Files.Sum(inf => inf.SizeInfo.Length));
+    public bool IsSelected { get; set; }
+}
