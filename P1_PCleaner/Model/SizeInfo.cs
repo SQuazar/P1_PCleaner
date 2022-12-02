a9a2023e94fba5002a420967b2b01e14281d89a0
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 20:45:05 +0300

    Added size info class

---
+++ P1_PCleaner/Model/SizeInfo.cs
@@ -0,0 +1,15 @@
+﻿using P1_PCleaner.Factory;
+
+namespace P1_PCleaner.Model;
+
+public class SizeInfo
+{
+    public double Size { get; set; }
+    public long Length { get; set; }
+    public string SizeSuffix { get; set; } = "B";
+    
+    public static SizeInfo operator +(SizeInfo src, SizeInfo dest)
+    {
+        return new FileSizeFactory().CreateInfo(src.Length + dest.Length);
+    }
+}
