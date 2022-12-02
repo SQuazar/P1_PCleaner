1f67b8c32a3cb0206dd6d96d2a27e4e9f608ccae
flaweoff <67743907+flaweoff@users.noreply.github.com>
2022-12-02 20:46:47 +0300

    Added new and utility method

--- P1_PCleaner/IO/IScanner.cs
+++ P1_PCleaner/IO/IScanner.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using P1_PCleaner.Factory;
 using P1_PCleaner.Model;
+using P1_PCleaner.Repository;
 
 namespace P1_PCleaner.IO;
 
 public interface IScanner
 {
-    List<FileInf> Scan();
+    IEnumerable<FileInf> Scan();
+
+    IFilesRepository.ScanCategory GetScanCategory();
+
+    static IEnumerable<FileInf> GetFiles(DirectoryInfo directory, string[] ext, bool deep = true)
+    {
+        if (directory == null) throw new ArgumentNullException(nameof(directory));
+        IFileSizeFactory factory = new FileSizeFactory();
+        return directory.EnumerateFiles("*.*", new EnumerationOptions
+                { RecurseSubdirectories = true, MatchType = MatchType.Win32, AttributesToSkip = 0 })
+            .Where(fileInfo => ext.Any(x => fileInfo.Extension.EndsWith(x) || x.Equals("*")))
+            .Select(fileInfo => new FileInf
+                { Path = fileInfo.FullName, SizeInfo = factory.CreateInfo(fileInfo.Length) });
+    }
 }
